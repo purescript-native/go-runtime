7f337c877b21eb6cfa2f7eeba8a1d1b941c3ca21
Andy Arvanitis <andyarvanitis@gmail.com>
2020-10-08 21:56:52 -0700

    Unique 'Undefined' value instance

--- purescript.go
+++ purescript.go
@@ -21,7 +21,8 @@ type Fn8 = func(Any, Any, Any, Any, Any, Any, Any, Any) Any
 type Fn9 = func(Any, Any, Any, Any, Any, Any, Any, Any, Any) Any
 type Fn10 = func(Any, Any, Any, Any, Any, Any, Any, Any, Any, Any) Any
 
-const Undefined = "\x00<undefined>\x00"
+var undefined = "<undefined>"
+var Undefined Any = &undefined
 
 var foreign = make(map[string]Dict)
 
