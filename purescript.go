0597741f0c339bc7c3e5f95fd7117f5ce0bb8280
Andy Arvanitis <andyarvanitis@gmail.com>
2020-10-08 21:53:56 -0700

    Remove debug vs release behavior (use descriptive panics)

--- purescript.go
+++ purescript.go
@@ -41,3 +41,49 @@ func Get(dict Dict, key string) Any {
 	}
 	return value
 }
+
+func Apply(f Any, args ...Any) Any {
+	result := f
+	for _, arg := range args {
+		fn := result.(Fn)
+		result = fn(arg)
+	}
+	return result
+}
+
+func Run(f Any, args ...Any) Any {
+	fn := f.(EffFn)
+	return fn()
+}
+
+func Contains(dict Any, key string) bool {
+	d := dict.(Dict)
+	_, found := d[key]
+	return found
+}
+
+func Is(dict Any, key string) bool {
+	d := dict.(Dict)
+	_, found := d[key]
+	return found
+}
+
+func Length(arr Any) Any {
+	a := arr.([]Any)
+	return len(a)
+}
+
+func Index(arr Any, idx Any) Any {
+	a := arr.([]Any)
+	i := idx.(int)
+	return a[i]
+}
+
+func CopyDict(dict Any) Any {
+	d := dict.(Dict)
+	cpy := make(Dict)
+	for k, v := range d {
+		cpy[k] = v
+	}
+	return cpy
+}
